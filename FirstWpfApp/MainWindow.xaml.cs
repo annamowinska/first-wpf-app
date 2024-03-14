13d9629dfc31800e2e242c6558550c5dfb0363c2
Anna Em <mowinska.ania@gmail.com>
2024-03-14 13:50:56 +0100

    MainWindow.xaml.cs cleanup and create MainViewModel.cs

--- FirstWpfApp/MainWindow.xaml.cs
+++ FirstWpfApp/MainWindow.xaml.cs
@@ -1,53 +1,13 @@
-﻿using System;
-using System.Diagnostics;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using FirstWpfApp.ViewModel;
 
-namespace ClickMe
+namespace FirstWpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
-        }
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            RotateTransform rotateTransform = new RotateTransform();
-            logoImage.RenderTransform = rotateTransform;
-            DoubleAnimation animation = new DoubleAnimation();
-            animation.From = 0;
-            animation.To = 360;
-            animation.Duration = new Duration(TimeSpan.FromSeconds(5));
-            animation.RepeatBehavior = RepeatBehavior.Forever;
-            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, animation);
-        }
-        private void LogoImage_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            Process.Start(new ProcessStartInfo("https://github.com/annamowinska/first_wpf_app")
-            {
-                UseShellExecute = true
-            });
-        }
-        private void MinimizeButtonClickEvent(object sender, RoutedEventArgs e)
-        {
-            mainWindow.WindowState = WindowState.Minimized;
-        }
-        private void CloseButtonClickEvent(object sender, RoutedEventArgs e)
-        {
-            mainWindow.Close();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ FirstWpfApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace FirstWpfApp.ViewModel
+{
+    public class MainViewModel : ObservableObject
+    {
+        public ICommand ExitCommand { get; }
+        public ICommand MinimizeCommand { get; }
+        public ICommand OpenUrlCommand { get; }
+
+        public MainViewModel()
+        {
+            ExitCommand = new RelayCommand(Exit);
+            MinimizeCommand = new RelayCommand(Minimize);
+            OpenUrlCommand = new RelayCommand(OpenUrl);
+        }
+
+        private void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        private void Minimize()
+        {
+            App.Current.MainWindow.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void OpenUrl()
+        {
+            Process.Start("https://github.com/annamowinska/first_wpf_app");
+        }
+    }
+}
